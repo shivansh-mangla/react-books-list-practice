67ea044220a1b163580b7d80d5853e9f2a5e997e
shivansh mangla <shivanshmangla72@gmail.com>
2026-01-19 15:38:17 +0530

    added routes and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import BooksPage1 from './pages/BooksPage1';
+import MainLayout from './layouts/MainLayout';
+import BooksPage2 from './pages/BooksPage2';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<BooksPage1/>} />
+        <Route path='/1' element={<BooksPage1/>} />
+        <Route path='/2' element={<BooksPage2/>} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/BooksPage2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BooksPage2 = () => {
+  return (
+    <div>BooksPage2</div>
+  )
+}
+
+export default BooksPage2
